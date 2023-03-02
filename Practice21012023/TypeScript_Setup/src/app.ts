7f3d9857ab3c4d65c6ac21349091fea16f791fdd
Marina <78799773+marinezh@users.noreply.github.com>
2023-03-02 21:44:42 +0200

    add some new tasks

--- Practice21012023/TypeScript_Setup/src/app.ts
+++ Practice21012023/TypeScript_Setup/src/app.ts
@@ -26,3 +26,50 @@ console.log(mult(5, 4));
 console.log(division(5, 0));
 console.log(division(500, 5));
 printOutput(add(5, 3));
+
+const hobbies = ["sleeping", "reading", "cooking"];
+
+const activeHobbies = ["hiking"];
+
+activeHobbies.push(hobbies[0], hobbies[1]);
+activeHobbies.push(...hobbies);
+
+console.log(activeHobbies);
+
+const person = {
+  name: "james",
+  age: 36,
+};
+
+const copiedPerson = { ...person };
+console.log(copiedPerson);
+
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const it = new Department("IT department");
+console.log(it);
+
+class Product {
+  title: string;
+  price: number;
+  private isListed: boolean;
+
+  constructor(name: string, pr: number) {
+    this.title = name;
+    this.price = pr;
+    this.isListed = true;
+  }
+}
+
+class Product1 {
+  private isListed: boolean;
+  constructor(public title: string, public price: number) {
+    this.isListed = true;
+  }
+}
