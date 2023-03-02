f1a973adf19eca196c36173d55f8f7e64c133443
Marina <78799773+marinezh@users.noreply.github.com>
2023-03-02 20:38:35 +0200

    add Location App

---
+++ LocationApp-main/src/app.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+console.log("Location app starts...");
+
+// Two different ways to specify type
+const form = <HTMLFormElement>document.querySelector("form")!;
+const address = document.querySelector("#address")! as HTMLInputElement;
+
+type GoogleGeoResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchLocationHandler(e: Event) {
+  e.preventDefault();
+  const location = address.value;
+
+  const GOOGLE_API_KEY = "INSERTYOURGOOGLEAPIKEY";
+
+  axios
+    .get<GoogleGeoResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        location
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 11,
+        }
+      );
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => console.log(err));
+}
+
+form.addEventListener("submit", searchLocationHandler);
